d9d05fda24fab8cfdc60e6efea5ed15bce1f60d2
Daniel Henrique Barboza <danielhb@linux.vnet.ibm.com>
2017-01-20 10:15:42 -0200

    /config/plugins: changing existing UI calls
    This patch changes ui/js/src/wok.api.js 'listPlugins' method
    to use the URL /config/plugins instead of /plugins.
    
    With this change, ui/js/src/wok.logos.js and ui/js/src/wok.main.js
    were also changed to handle the different return value from the
    /config/plugins API.
    
    Signed-off-by: Daniel Henrique Barboza <danielhb@linux.vnet.ibm.com>
    Message-Id: <1484914544-6132-8-git-send-email-dhbarboza82@gmail.com>

--- ui/js/src/wok.main.js
+++ ui/js/src/wok.main.js
@@ -1,7 +1,7 @@
 /*
  * Project Wok
  *
- * Copyright IBM Corp, 2015-2016
+ * Copyright IBM Corp, 2015-2017
  *
  * Code derived from Project Kimchi
  *
@@ -143,16 +143,20 @@ wok.main = function() {
         var tabs = retrieveTabs('wok', wokConfigUrl);
         wok.listPlugins(function(plugins) {
             $(plugins).each(function(i, p) {
+                if (p.enabled === false) {
+                    return true;
+                }
+
                 var url = wok.substitute(pluginConfigUrl, {
-                    plugin: p
+                    plugin: p.name
                 });
                 var i18nUrl = wok.substitute(pluginI18nUrl, {
-                    plugin: p
+                    plugin: p.name
                 });
                 wok.getI18n(function(i18nObj){ $.extend(i18n, i18nObj)},
                             function(i18nObj){ //i18n is not define by plugin
                             }, i18nUrl, true);
-                var pluginTabs = retrieveTabs(p, url);
+                var pluginTabs = retrieveTabs(p.name, url);
                 if(pluginTabs.length > 0){
                     tabs.push.apply(tabs, pluginTabs);
                 }
